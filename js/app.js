4afe208ebffbbd4c6a91052ebada72776820b015
Lekso Duchidze <leksoduchi@gmail.com>
2022-06-26 23:29:41 +0400

    add css, js, html

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// პირველი
+let myarr = [3, 5, 7, 9, 11];
+let a1 = myarr[0];
+let a2 = myarr[1];
+let a3 = myarr[2];
+let a4 = myarr[3];
+let a5 = myarr[4];
+
+let average = (a1 + a2 + a3 + a4 + a5) / myarr.length;
+console.log(average);
+
+//მეორე (ასო ბგერა 'ლ')
